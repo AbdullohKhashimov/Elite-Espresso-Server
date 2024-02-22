edd5436ef8d4a7f54275b388da05782c51dfe626
Abdulloh Salimov <abdullohsalimov5@gmail.com>
2024-02-23 04:48:54 +0900

    feat: connect mongoDB via Mongoose

--- src/server.ts
+++ src/server.ts
@@ -16,5 +16,14 @@
 import dotenv from "dotenv";
 dotenv.config();
 
-console.log("PORT:", process.env.PORT);
-console.log("MONGO_URL:", process.env.MONGO_URL);
+import mongoose from "mongoose";
+
+mongoose
+  .connect(process.env.MONGO_URL as string, {})
+  .then((data) => {
+    console.log("MongoDB connection succed");
+    const PORT = process.env.PORT ?? 3003;
+  })
+  .catch((err) => {
+    console.log("Error on connection MongoDB", err);
+  });
