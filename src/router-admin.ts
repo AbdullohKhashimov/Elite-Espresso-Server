1f6b63f24c644554c71768fb58c48ff19d460ad6
Abdulloh Salimov <abdullohsalimov5@gmail.com>
2024-03-07 06:38:28 +0900

    feat: create product controller and product routing

--- src/router-admin.ts
+++ src/router-admin.ts
@@ -1,6 +1,7 @@
 import express from "express";
 const routerAdmin = express.Router();
 import restaurantController from "./controllers/restaurant.controller";
+import productController from "./controllers/product.controllers";
 
 // gets two argument =>
 // 1: endpoint (url)
@@ -19,6 +20,10 @@ routerAdmin.get("/logout", restaurantController.logout);
 routerAdmin.get("/check-me", restaurantController.checkAuthSession);
 
 /** Product **/
+
+routerAdmin.get("/product/all", productController.getAllProducts);
+routerAdmin.post("/product/create", productController.createNewProduct);
+routerAdmin.post("/product/:id", productController.updateChosenProduct);
 
 /** User **/
 
