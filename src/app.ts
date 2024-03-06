06bc14dc610808048f8813483cc4dc6e010ec9a5
Abdulloh Salimov <abdullohsalimov5@gmail.com>
2024-03-07 00:07:57 +0900

    feat: integrate middleware sessions

--- src/app.ts
+++ src/app.ts
@@ -5,6 +5,15 @@ import routerAdmin from "./router-admin";
 import morgan from "morgan";
 import { MORGAN_FORMAT } from "./libs/config";
 
+import session from "express-session";
+import ConnectMongoDB from "connect-mongodb-session";
+
+const MongoDBStore = ConnectMongoDB(session);
+const store = new MongoDBStore({
+  uri: String(process.env.MONGO_URL),
+  collection: "sessions",
+});
+
 /** 1- Entrance **/
 const app = express();
 // middleware pattern
@@ -14,6 +23,17 @@ app.use(express.json());
 app.use(morgan(MORGAN_FORMAT));
 
 /** 2- Session **/
+app.use(
+  session({
+    secret: String(process.env.SESSION_SECRET),
+    cookie: {
+      maxAge: 1000 * 3600 * 3, // 3 hours
+    },
+    store: store,
+    resave: true,
+    saveUninitialized: true,
+  })
+);
 
 /** 3- Views **/
 app.set("views", path.join(__dirname, "views"));
