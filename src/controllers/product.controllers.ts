b02b7799bdb8972630dcb39e51e786fe96044bab
Abdulloh Salimov <abdullohsalimov5@gmail.com>
2024-03-21 01:11:12 +0900

    fix: modify products page based on product data

--- src/controllers/product.controllers.ts
+++ src/controllers/product.controllers.ts
@@ -17,6 +17,7 @@ productController.getAllProducts = async (req: Request, res: Response) => {
   try {
     console.log("getAllProducts");
     const data = await productService.getAllProducts();
+    console.log("products:", data);
 
     // renderdagi ikkinchi argument qanday nom bilan yuvorish
     res.render("products", { products: data });
