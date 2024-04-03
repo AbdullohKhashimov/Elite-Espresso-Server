a97765757ebc41dcbac72f887b42e71d85fc89d5
Abdulloh Salimov <abdullohsalimov5@gmail.com>
2024-04-04 06:02:20 +0900

    feat: develop getMemberDetail method

--- src/controllers/member.controller.ts
+++ src/controllers/member.controller.ts
@@ -67,9 +67,25 @@ memberController.login = async (req: Request, res: Response) => {
 
 memberController.logout = (req: ExtendedRequest, res: Response) => {
   try {
-    console.log("logout");
+    console.log("getMemberDetail");
     res.cookie("accessToken", null, { maxAge: 0, httpOnly: true });
     res.status(HttpCode.OK).json({ logout: true });
+  } catch (err) {
+    console.log("Error, logout");
+    if (err instanceof Errors) res.status(err.code).json(err);
+    else res.status(Errors.standard.code).json(Errors.standard);
+  }
+};
+
+memberController.getMemberDetail = async (
+  req: ExtendedRequest,
+  res: Response
+) => {
+  try {
+    console.log("logout");
+    const result = await memberService.getMemberDetail(req.member);
+
+    res.status(HttpCode.OK).json(result);
   } catch (err) {
     console.log("Error, logout");
     if (err instanceof Errors) res.status(err.code).json(err);
